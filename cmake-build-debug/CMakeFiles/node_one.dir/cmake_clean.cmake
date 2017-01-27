f3b4d8947ee926ad83eead147b2779f6469f0e38
sesovero <sebastian@otherlab.com>
2017-01-27 12:11:33 -0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/node_one.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node_one.dir/main.cpp.o"
+  "node_one.pdb"
+  "node_one"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_one.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
